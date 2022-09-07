66bc06db53b935adddf491bfa181bd4c6b1f24e7
NirShihor <nir@differentdog.com>
2022-09-07 21:20:15 +0100

    finished tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    title: `tutorial_gatsby_docs`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: `tutorial_gatsby_docs`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+};
